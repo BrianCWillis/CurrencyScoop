c6f6394119bc065b733f27b229f7553c7a357978
BrianCWillis <31476209+BrianCWillis@users.noreply.github.com>
2021-06-21 11:59:17 -0500

    Add files via upload

--- CurrencyScoop/scripts/CurrencyScoop.js
+++ CurrencyScoop/scripts/CurrencyScoop.js
@@ -1,13 +1,17 @@
+//Element Containers.
 var e = React.createElement;
 var content = [
 	e('p', {className: 'chart-heading', key: 'currency-heading'}, 'Name of Currency'),
 	e('p', {className: 'chart-heading', key: 'country-heading'}, 'Country/Countries Using It')
 ];
+
+//Web service request info and response container.
 var currencies = {};
 const endpoint = 'currencies';
 const key = '20313d0bc44c01c2f60c711102e67deb';
 const type = 'fiat';
 
+//ajax request.
 //Currently, this fails.
 $.ajax({
 	url: 'https://api.currencyscoop.com/v1/' + endpoint + '?api_key=' + key + '&type=' + type,
@@ -17,6 +21,7 @@ $.ajax({
 	}
 });
 
+//Proof-of-concept data (to show that the ReactJS code works).
 currencies = {
 	"quotes":[
 		{"currency": "Dollar", "countries": "USA, Canada, Australia, New Zealand"},
@@ -28,6 +33,7 @@ currencies = {
 	]
 };
 
+//Populate content with table data.
 var currencyID, countryID;
 for(var i=0; i<currencies.quotes.length; i++){
 	currencyID = 'currency'+i.toString();
